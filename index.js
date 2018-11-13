2c379b9dcaf5dcb9b90459518463b4177d42bccc
Angie Quin <aquinones100@gmail.com>
2018-11-13 20:23:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,6 @@
+katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  console.log ("Welcome, + `${name}` + you are number + katzDeliLine.length + in line")
+}
