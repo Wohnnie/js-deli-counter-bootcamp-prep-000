c84fae2317e77e17e0822d33128a0d4e7cd09e89
Angie Quin <aquinones100@gmail.com>
2018-11-13 20:35:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function takeANumber(katzDeliLine, name){
   console.log ("Welcome, $`{name}` You are number + katzDeliLine.length +in line")
 }
 
-takeANumber(katzDeliLine, Ada);
-takeANumber(katzDeliLine, Steven);
+takeANumber(katzDeliLine,  Ada);
+takeANumber(katzDeliLine,  Steven);
 
 function nowServing(katzDeliLine){
   if(katzDeliLine.length===0){
@@ -23,7 +23,7 @@ function nowServing(katzDeliLine){
     if(line.length===0) {
       return "The line is currently empty.";}
       else{
-         return "The line is currently: 1. " + line[0] + ","+ " 2. " + line[1] +"," + " 3. " + line[2];
+        return The line is currently: "1. " + line[0] + "," + "2. " + line [1] = "," + "3. " + line [2];
       }
 
     }
